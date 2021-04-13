92d16b5d7e790b360aa772ae40fe49bd4029707b
Cleyton Fábio <cleyton.flb@gmail.com>
2021-04-12 21:56:15 -0300

    UPDATE: metodo iniciarJogo() rodando

--- src/JogoDescobrePratos.java
+++ src/JogoDescobrePratos.java
@@ -16,31 +16,8 @@ public class JogoDescobrePratos {
 		JOptionPane.showMessageDialog(null, "Seja Bem Vindo ao Descubra o Prato.\n Pressione Ok para continuar.");
 		JOptionPane.showMessageDialog(null, "Na minha memória eu conheço estes pratos: " + pratos.getListaDePratos());
 		
-		int liga_desliga = JOptionPane.showConfirmDialog(null, "Vou te tentar adivinhar o prato ques esta pensando.", "Gostaria de Iniciar o Jogo?", JOptionPane.YES_OPTION, JOptionPane.QUESTION_MESSAGE);
-		
-		while (liga_desliga == 0) {
-			
-			int resposta = JOptionPane.showConfirmDialog(null, "Seria um " + getTentativaDeAdivinhar() + " ?", "Qual prato você esta pensando?", JOptionPane.YES_NO_OPTION);
-			
-			if (resposta == 0) {
-				JOptionPane.showMessageDialog(null, "Acertei!");
-				liga_desliga = 1;
-				JOptionPane.showMessageDialog(null, "Fim, para continuar reinicie o jogo");
-			}
-			
-			else if (pratos.getQuantidadeDePratos() == 0) {
-				JOptionPane.showMessageDialog(null, "Eu não tenho mais nenhuma ideia de prato.");
-				int add_nova_prato = JOptionPane.showConfirmDialog(null, "Gostaria de Adicionar algum prato novo?", "Gostaria de Iniciar o Jogo?", JOptionPane.YES_OPTION, JOptionPane.QUESTION_MESSAGE);
-				if (add_nova_prato == 0) {
-					pratos.colocarNovoPratoNaLista("prato");
-					
-				}
-			} else {
-				JOptionPane.showMessageDialog(null, "Errei!");
-			}
-		}
-		
-		
+		int liga_desliga = JOptionPane.showConfirmDialog(null, "Gostaria de Iniciar o Jogo?", "Jogo: Eu sei oque você quer comer!", JOptionPane.YES_OPTION, JOptionPane.QUESTION_MESSAGE);		
+		iniciarJogo(liga_desliga);
 		//JOptionPane.showMessageDialog(null, args);
 		
 		//while (status == true) {
@@ -64,6 +41,31 @@ public class JogoDescobrePratos {
 		//JOptionPane.showMessageDialog(null, pratos.getListaDePratos());
 	}
 
+	public static void iniciarJogo(int liga_desliga) {
+		while (liga_desliga  == 0) {			
+			int resposta = JOptionPane.showConfirmDialog(null, "Seria um " + getTentativaDeAdivinhar() + " ?", "Qual prato você esta pensando?", JOptionPane.YES_NO_OPTION);
+
+			if (resposta == 0) {
+				JOptionPane.showMessageDialog(null, "Acertei!");
+				status = 1;
+				JOptionPane.showMessageDialog(null, "Fim, para continuar reinicie o jogo");
+			}
+			
+			else if (pratos.getQuantidadeDePratos() == 0) {
+				JOptionPane.showMessageDialog(null, "Eu não tenho mais nenhuma ideia de prato.");
+				int add_nova_prato = JOptionPane.showConfirmDialog(null, "Gostaria de Adicionar algum prato novo?", "Gostaria de Iniciar o Jogo?", JOptionPane.YES_OPTION, JOptionPane.QUESTION_MESSAGE);
+
+				if (add_nova_prato == 0) {
+					pratos.colocarNovoPratoNaLista("prato");
+					
+				}
+			} else {
+				JOptionPane.showMessageDialog(null, "Errei!");
+			}
+		}
+
+	}
+	
 	public Boolean getStatus() { 
 		if (status == false) {
 			return false;
